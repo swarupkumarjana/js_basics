b4f381721ab9c40a48e3491a12ef687530daa221
Swarup Kumar Jana <145488946+swarupkumarjana@users.noreply.github.com>
2023-11-24 04:37:42 +0000

    Learn Variables in JS

---
+++ first_js/variable.js
@@ -0,0 +1,9 @@
+const accountHolder= "Swarup Kumar Jana"
+let accountPassword= 3210
+let accountCity= "Haldia"
+let accountState
+
+/* dont use var */
+
+console.log(accountHolder);
+console.table([accountCity, accountHolder, accountPassword, accountState])
