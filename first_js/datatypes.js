83c3c7ca2c44a45b2c8a01037fe6d61bb10d9bc8
Swarup Kumar Jana <145488946+swarupkumarjana@users.noreply.github.com>
2023-11-25 15:46:55 +0000

    learned about datatypes

---
+++ first_js/datatypes.js
@@ -0,0 +1,20 @@
+const accountHolder= "Swarup Kumar Jana"
+let accountPassword= 3210
+let accountCity= "Haldia"
+let accountState
+
+// colsole.log(typeof ("accountHolder"))
+
+
+const numbers= [1,2,3,4,5]
+const students=["bumba", "subha", "dipti"]
+
+console.log(typeof students);
+
+
+let score= 23
+console.log( typeof score);
+
+//empty array- []
+// const userId="3210"
+// empty object- {}
